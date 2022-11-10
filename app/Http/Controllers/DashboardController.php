58782630a30dd40249580a26dd23ec22e8cdc8f0
elijah97 <elie.gash42@gmail.com>
2022-11-10 10:30:39 +0200

    Some analytics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,94 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.analytics');
+        $all_applications = DB::select(
+            DB::raw(
+                "SELECT *
+                 FROM applications"
+            )
+        );
+
+        $reviewed_applications = DB::select(
+            DB::raw(
+                "SELECT *
+                 FROM applications
+                 WHERE reviewStatus = 1
+                 "
+            )
+        );
+
+        $approved_applications = DB::select(
+            DB::raw(
+                "SELECT *
+                 FROM applications
+                 WHERE approveStatus = 1
+                 AND reviewStatus = 1
+                 "
+            )
+        );
+
+        $revoked_applications = DB::select(
+            DB::raw(
+                "SELECT *
+                 FROM applications
+                 WHERE approveStatus = 2
+                 "
+            )
+        );
+
+        $pending_applications = DB::select(
+            DB::raw(
+                "SELECT *
+                 FROM applications
+                 WHERE reviewStatus = 0
+                 "
+            )
+        );
+        $current_date = date('Y-m-d');
+        $current_budget = DB::select(
+            DB::raw(
+                "SELECT *
+                 FROM budgets
+                 WHERE '$current_date' BETWEEN dateFrom and dateTo 
+                 "
+            )
+        );
+
+        $total_users = DB::select(
+            DB::raw(
+                "SELECT *
+                 FROM users 
+                 "
+            )
+        );
+
+        $total_categories = DB::select(
+            DB::raw(
+                "SELECT *
+                 FROM categories 
+                 "
+            )
+        );
+
+        // return $current_budget;
+        return view(
+            'dashboard.analytics',
+            [
+                'all_applications' => $all_applications,
+                'reviewed_applications' => $reviewed_applications,
+                'approved_applications' => $approved_applications,
+                'revoked_applications' => $revoked_applications,
+                'pending_applications' => $pending_applications,
+                'current_budget' => $current_budget,
+                'total_users' => $total_users,
+                'total_categories' => $total_categories,
+            ]
+        );
     }
 }
--- resources/views/dashboard/analytics.blade.php
+++ resources/views/dashboard/analytics.blade.php
@@ -1,68 +1,157 @@
 @extends('dashboard.sidemenu')
 @section('content')
+    <div class="container-fluid Main_Content pad32">
+        <div class="row">
 
-<div class="container-fluid Main_Content pad32">
-    <div class="row">
+            <div class="col-12">
+                <div class="row">
 
-        <div class="col-12">
-            <div class="row">
-                <div class="col-3">
-                    <div class="col-12 M_Card_16 mabo32">
-                        <p class="M_Txt_Large text-uppercase green"> 100</p>
-                        <p class="M_Txt_Info grey"> Subtitle</p>
+                    <div class="col-3">
+                        <div class="row col-12 M_Card_16 mabo32">
+                            <div class="col-2">
+                                <span class="material-icons analyticsIcon">
+                                    attach_money
+                                </span>
+                            </div>
+                            <div class="col-10">
+
+                                <p class="M_Txt_Large text-uppercase green">
+                                    {{ number_format($current_budget[0]->amount + $current_budget[0]->expenses) }} Rwf</p>
+                                <p class="M_Txt_Info grey"> This Month's budget</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-3">
-                    <div class="col-12 M_Card_16 mabo32">
-                        <p class="M_Txt_Large text-uppercase green"> 100</p>
-                        <p class="M_Txt_Info grey"> Subtitle</p>
+                    <div class="col-3">
+                        <div class="row col-12 M_Card_16 mabo32">
+                            <div class="col-2">
+                                <span class="material-icons analyticsIcon">
+                                    payment
+                                </span>
+                            </div>
+                            <div class="col-10">
+                                <p class="M_Txt_Large text-uppercase green">
+                                    {{ number_format($current_budget[0]->expenses) }} Rwf</p>
+                                <p class="M_Txt_Info grey"> This Month's Expenses</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-3">
-                    <div class="col-12 M_Card_16 mabo32">
-                        <p class="M_Txt_Large text-uppercase green"> 100</p>
-                        <p class="M_Txt_Info grey"> Subtitle</p>
+
+                    <div class="col-3">
+                        <div class="row col-12 M_Card_16 mabo32">
+                            <div class="col-2">
+                                <span class="material-icons analyticsIcon">
+                                    inbox
+                                </span>
+                            </div>
+                            <div class="col-10">
+                                <p class="M_Txt_Large text-uppercase green"> {{ count($all_applications) }}</p>
+                                <p class="M_Txt_Info grey"> Total Applications</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-3">
-                    <div class="col-12 M_Card_16 mabo32">
-                        <p class="M_Txt_Large text-uppercase green"> 100</p>
-                        <p class="M_Txt_Info grey"> Subtitle</p>
+
+                    <div class="col-3">
+                        <div class="row col-12 M_Card_16 mabo32">
+                            <div class="col-2">
+                                <span class="material-icons analyticsIcon">
+                                    pending_actions
+                                </span>
+                            </div>
+                            <div class="col-10">
+                                <p class="M_Txt_Large text-uppercase green"> {{ count($pending_applications) }}</p>
+                                <p class="M_Txt_Info grey"> Pending Applications</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-3">
-                    <div class="col-12 M_Card_16 mabo32">
-                        <p class="M_Txt_Info grey pad32"> Graph</p>
+                    <div class="col-3">
+                        <div class="row col-12 M_Card_16 mabo32">
+                            <div class="col-2">
+                                <span class="material-icons analyticsIcon">
+                                    fact_check
+                                </span>
+                            </div>
+                            <div class="col-10">
+                                <p class="M_Txt_Large text-uppercase green"> {{ count($approved_applications) }}</p>
+                                <p class="M_Txt_Info grey"> Approved Applications</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-5">
-                    <div class="col-12 M_Card_16 mabo32">
-                        <p class="M_Txt_Info grey pad32"> Graph</p>
+                    <div class="col-3">
+                        <div class="row col-12 M_Card_16 mabo32">
+                            <div class="col-2">
+                                <span class="material-icons analyticsIcon">
+                                    playlist_remove
+                                </span>
+                            </div>
+                            <div class="col-10">
+                                <p class="M_Txt_Large text-uppercase green"> {{ count($revoked_applications) }}</p>
+                                <p class="M_Txt_Info grey"> Revoked Applications</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-2">
-                    <div class="col-12 M_Card_16 mabo32">
-                        <p class="M_Txt_Info grey pad32"> Graph</p>
+                    <div class="col-3">
+                        <div class="row col-12 M_Card_16 mabo32">
+                            <div class="col-2">
+                                <span class="material-icons analyticsIcon">
+                                    group
+                                </span>
+                            </div>
+                            <div class="col-10">
+                                <p class="M_Txt_Large text-uppercase green">
+                                    {{ count($total_users) }}</p>
+                                <p class="M_Txt_Info grey"> Total Users</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-2">
-                    <div class="col-12 M_Card_16 mabo32">
-                        <p class="M_Txt_Info grey pad32"> Graph</p>
+                    <div class="col-3">
+                        <div class="row col-12 M_Card_16 mabo32">
+                            <div class="col-2">
+                                <span class="material-icons analyticsIcon">
+                                    category
+                                </span>
+                            </div>
+                            <div class="col-10">
+                                <p class="M_Txt_Large text-uppercase green">
+                                    {{ count($total_categories) }}</p>
+                                <p class="M_Txt_Info grey"> Available Categories</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
+                    <div class="col-3">
+                        <div class="col-12 M_Card_16 mabo32">
+                            <p class="M_Txt_Info grey pad32"> Graph</p>
+                        </div>
+                    </div>
+
+                    <div class="col-5">
+                        <div class="col-12 M_Card_16 mabo32">
+                            <p class="M_Txt_Info grey pad32"> Graph</p>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="col-12 M_Card_16 mabo32">
+                            <p class="M_Txt_Info grey pad32"> Graph</p>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="col-12 M_Card_16 mabo32">
+                            <p class="M_Txt_Info grey pad32"> Graph</p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
+
         </div>
-
     </div>
-</div>
-
-
 @endsection
